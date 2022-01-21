9b0a6af83f2226c9b9fcb24f324a839430c67f48
Gesporidgers <konkord302@gmail.com>
2022-01-21 18:07:00 +0300

    Adding documentation

--- TeoModule.py
+++ TeoModule.py
@@ -1,13 +1,25 @@
 def numsysCon(number, system):
+    """Converts number to a specified number system
+
+    :param number: original number
+    :param system: necessary number system
+    :return: Conversed number
+    """
     result = ''
     while number != 0:
         t = str(number % system)
         result = t + result
-        a = number // system
+        number = number // system
     return result
 
 
 def zeronumber(number, system):
+    """Counts '0' in number in specified number system
+
+    :param number: original number
+    :param system: necessary number system
+    :return: Quantity '0'
+    """
     t = numsysCon(number, system)
     return t.count('0')
 
--- Testing module.py
+++ Testing module.py
@@ -1,2 +1,3 @@
 import TeoModule
 print(TeoModule.numsysCon(564, 5))
+print(TeoModule.zeronumber(564,5))
