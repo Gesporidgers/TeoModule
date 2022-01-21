4201c73d1612188cc22820e494f2d4e094a5bd60
Gesporidgers <konkord302@gmail.com>
2022-01-21 17:53:02 +0300

    Reformatting

--- TeoModule.py
+++ TeoModule.py
@@ -1,17 +1,15 @@
-def numsysCon(a,s):
-	result=''
-	while a != 0:
-		t = str(a % s)
-		result = t + result
-		a = a // s
-	return result
-	
-	
-def zeronumber(a,s):
-	t = numsysCon(a,s)
-	return t.count('0')
-	
-	
+def numsysCon(a, s):
+    result = ''
+    while a != 0:
+        t = str(a % s)
+        result = t + result
+        a = a // s
+    return result
+
+
+def zeronumber(a, s):
+    t = numsysCon(a, s)
+    return t.count('0')
 
 
 print('---===TeoModule===---')
